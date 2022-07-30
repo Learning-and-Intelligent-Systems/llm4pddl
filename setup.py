d2241daa0802d78b42e33e409f0c5f200d4b0419
Tom Silver <tomssilver@gmail.com>
2022-07-30 10:31:04 -0400

    setup mypy

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='llm4pddl',
-    version='0.1.0',
-    packages=find_packages(include=['llm4pddl', 'llm4pddl.*']),
+    name="llm4pddl",
+    version="0.1.0",
+    packages=find_packages(include=["llm4pddl", "llm4pddl.*"]),
     install_requires=[
-        'absl-py',
-        'PyYAML',
+        "absl-py",
+        "PyYAML",
     ],
+    extras_require={"develop": [
+        "mypy",
+        "pytest-cov>=2.12.1",
+        "pytest-pylint>=0.18.0",
+        "yapf==0.32.0",
+        "docformatter",
+        "isort"
+    ]}
 )
 
