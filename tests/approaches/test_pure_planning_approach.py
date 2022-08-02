c1be8847b297397325e698ecab2030ae36232e20
Tom Silver <tomssilver@gmail.com>
2022-08-02 16:36:15 -0400

    minor

--- tests/approaches/test_pure_planning_approach.py
+++ tests/approaches/test_pure_planning_approach.py
@@ -7,10 +7,10 @@ from llm4pddl.envs import create_env
 
 def test_pure_planning_approach():
     """Tests for PurePlanningApproach()."""
-    utils.reset_flags({"num_train_tasks": 0, "num_eval_tasks": 3})
+    utils.reset_flags({"num_train_tasks": 0, "num_eval_tasks": 2})
     approach = PurePlanningApproach()
     env = create_env("pyperplan-blocks")
     for task in env.get_eval_tasks():
         plan = approach.solve(task)
         assert plan is not None
-        assert utils.is_valid_plan(task, plan)
+        assert utils.validate_plan(task, plan)
