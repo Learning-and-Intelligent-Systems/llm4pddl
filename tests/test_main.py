b96939d9cab740b3e97c98efdf22a027dd7fb2bf
Tom Silver <tomssilver@gmail.com>
2022-08-02 17:29:26 -0400

    update main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,22 @@
 """Tests for main.py."""
 
+import shutil
 import sys
+import tempfile
 
 from llm4pddl.main import _main
 
 
 def test_main():
     """Tests for main.py."""
-    sys.argv = ["dummy", "--env", "my_env", "--approach", "my_approach"]
+    # Create a temporary results dir so as to not pollute real results.
+    temp_results_dir = tempfile.TemporaryDirectory().name
+    # Test successful pipeline run with pure planning in blocks.
+    sys.argv = [
+        "dummy", "--env", "pyperplan-blocks", "--approach", "pure-planning",
+        "--num_train_tasks", "0", "--num_eval_tasks", "2", "--results_dir",
+        temp_results_dir
+    ]
     _main()  # should run
+    # Remove temporary results dir.
+    shutil.rmtree(temp_results_dir)
