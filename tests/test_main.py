1d76b4a564a1a2c08fd932d31e17af08ddb8116d
Tom Silver <tomssilver@gmail.com>
2022-08-02 17:37:01 -0400

    test coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,10 @@ import shutil
 import sys
 import tempfile
 
-from llm4pddl.main import _main
+from llm4pddl import utils
+from llm4pddl.approaches.base_approach import BaseApproach
+from llm4pddl.envs import create_env
+from llm4pddl.main import _main, _run_evaluation
 
 
 def test_main():
@@ -20,3 +23,37 @@ def test_main():
     _main()  # should run
     # Remove temporary results dir.
     shutil.rmtree(temp_results_dir)
+
+
+def test_run_evaluation():
+    """Tests for _run_evaluation() in main.py."""
+    utils.reset_flags({"num_train_tasks": 0, "num_eval_tasks": 2})
+
+    # Create a mock learning-based approach.
+    class _MockApproach(BaseApproach):
+
+        def __init__(self, plan_sequence):
+            self.remaining_plans = plan_sequence
+
+        @property
+        def is_learning_based(self) -> bool:
+            return True
+
+        def get_name(self):
+            return "dummy"
+
+        def solve(self, task):
+            return self.remaining_plans.pop(0)
+
+    # Cover cases where approach is None or invalid.
+    plan_sequence = [None, []]
+    approach = _MockApproach(plan_sequence)
+    env = create_env("pyperplan-blocks")
+    eval_tasks = env.get_eval_tasks()
+    results = _run_evaluation(approach, eval_tasks)
+    assert results == {
+        "num_eval_tasks": 2,
+        "num_successes": 0,
+        "num_no_solution": 1,
+        "num_invalid_plans": 1,
+    }
