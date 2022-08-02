935266f94b6e7963eb828fb759eaab767c723054
Tom Silver <tomssilver@gmail.com>
2022-08-02 17:41:36 -0400

    test coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ import tempfile
 from llm4pddl import utils
 from llm4pddl.approaches.base_approach import BaseApproach
 from llm4pddl.envs import create_env
-from llm4pddl.main import _main, _run_evaluation
+from llm4pddl.main import _main, _run_evaluation, _run_pipeline
 
 
 def test_main():
@@ -25,29 +25,50 @@ def test_main():
     shutil.rmtree(temp_results_dir)
 
 
-def test_run_evaluation():
-    """Tests for _run_evaluation() in main.py."""
-    utils.reset_flags({"num_train_tasks": 0, "num_eval_tasks": 2})
+class _MockApproach(BaseApproach):
 
-    # Create a mock learning-based approach.
-    class _MockApproach(BaseApproach):
+    def __init__(self, plan_sequence):
+        self.remaining_plans = plan_sequence
 
-        def __init__(self, plan_sequence):
-            self.remaining_plans = plan_sequence
+    @property
+    def is_learning_based(self) -> bool:
+        return True
 
-        @property
-        def is_learning_based(self) -> bool:
-            return True
+    def get_name(self):
+        return "dummy"
 
-        def get_name(self):
-            return "dummy"
+    def solve(self, task):
+        return self.remaining_plans.pop(0)
 
-        def solve(self, task):
-            return self.remaining_plans.pop(0)
 
+def test_run_pipeline():
+    """Tests for _run_pipeline() in main.py."""
+    # Create a temporary results dir so as to not pollute real results.
+    temp_results_dir = tempfile.TemporaryDirectory().name
+    utils.reset_flags({
+        "env": "pyperplan-blocks",
+        "approach": "dummy",
+        "num_train_tasks": 0,
+        "num_eval_tasks": 2,
+        "results_dir": temp_results_dir
+    })
     # Cover cases where approach is None or invalid.
     plan_sequence = [None, []]
     approach = _MockApproach(plan_sequence)
+    assert approach.get_name() == "dummy"
+    env = create_env("pyperplan-blocks")
+    _run_pipeline(approach, env)
+    # Remove temporary results dir.
+    shutil.rmtree(temp_results_dir)
+
+
+def test_run_evaluation():
+    """Tests for _run_evaluation() in main.py."""
+    utils.reset_flags({"num_train_tasks": 0, "num_eval_tasks": 2})
+    # Cover cases where approach is None or invalid.
+    plan_sequence = [None, []]
+    approach = _MockApproach(plan_sequence)
+    assert approach.get_name() == "dummy"
     env = create_env("pyperplan-blocks")
     eval_tasks = env.get_eval_tasks()
     results = _run_evaluation(approach, eval_tasks)
