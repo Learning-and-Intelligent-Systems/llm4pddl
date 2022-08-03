dd2e1e681bf759812b8630d55e46ce714aaaa945
Tom Silver <tomssilver@gmail.com>
2022-08-03 09:36:55 -0400

    create results analysis script

--- llm4pddl/flags.py
+++ llm4pddl/flags.py
@@ -12,6 +12,7 @@ def parse_flags() -> None:
     parser.add_argument("--env", required=True, type=str)
     parser.add_argument("--approach", required=True, type=str)
     parser.add_argument("--seed", required=True, type=int)
+    parser.add_argument("--experiment_id", default="", type=str)
     parser.add_argument("--num_train_tasks", default=5, type=int)
     parser.add_argument("--num_eval_tasks", default=10, type=int)
     parser.add_argument("--results_dir", default="results", type=str)
