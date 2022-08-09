5f652b16861100e7efd4117ed3924cef4e849c54
Reece Shuttleworth <70421122+reeceshuttle@users.noreply.github.com>
2022-08-09 17:07:55 -0400

    Integrate custom methods (#27)

---
+++ tests/envs/test_custom_env.py
@@ -0,0 +1,21 @@
+"""Tests custom_env.py."""
+
+from llm4pddl import utils
+from llm4pddl.envs.custom_env import CustomEnv
+
+
+def test_custom_env():
+    """Tests for CustomEnv()"""
+    utils.reset_flags({"num_train_tasks": 5, "num_eval_tasks": 10})
+    custom_envs = ['dressed']
+    for env_name in custom_envs:
+        env = CustomEnv(env_name)
+        assert env.get_name() == f'custom-{env_name}'
+        training_tasks = env.get_train_tasks()
+        assert len(training_tasks) == 5
+        eval_tasks = env.get_eval_tasks()
+        assert len(eval_tasks) == 10
+        for task_num, train_task in enumerate(training_tasks):
+            task_num += 1
+            assert train_task.domain_file == utils.get_custom_task(
+                env_name, task_num).domain_file
