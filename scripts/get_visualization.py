468736884615442e96647b202b746033179b91c9
Alchemy5 <varunhpr5@gmail.com>
2022-08-25 21:23:12 -0400

    simplified version no baselines vs. customs dictionaries

--- scripts/get_visualization.py
+++ scripts/get_visualization.py
@@ -1,12 +1,13 @@
 """Creates a collection of graphs summarizing results saved to a directory."""
 import argparse
-from typing import Dict, Tuple
+from typing import Dict
 
 import matplotlib.pyplot as plt
 import pandas as pd
 
-CUSTOM_APPROACHES = ["llm-standard-plan", "llm-multi", "llm-multi-plan"]
-BASELINE_APPROACHES = ["fd", "pyperplan"]
+APPROACHES = [
+    "llm-standard-plan", "llm-multi", "llm-multi-plan", "fd", "pyperplan"
+]
 
 
 def _main() -> None:
@@ -14,26 +15,20 @@ def _main() -> None:
     parser.add_argument("--input_path", type=str)
     parser.add_argument("--output_path", type=str)
     args = parser.parse_args()
-    custom_approaches, baseline_approaches = _construct_dictionaries(
-        args.input_path)
-    _get_visualization(custom_approaches, baseline_approaches,
-                       args.output_path)
+    approaches = _construct_dictionaries(args.input_path)
+    _get_visualization(approaches, args.output_path)
 
 
-def _construct_dictionaries(input_path: str) -> Tuple[Dict, Dict]:
-    custom_approaches: Dict = {}
-    for name in CUSTOM_APPROACHES:
-        custom_approaches[name] = {}
-    baseline_approaches: Dict = {}
-    for name in BASELINE_APPROACHES:
-        baseline_approaches[name] = {}
+def _construct_dictionaries(input_path: str) -> Dict[str, Dict[str, str]]:
+    approaches: Dict = {}
+    for name in APPROACHES:
+        approaches[name] = {}
 
     df = pd.read_csv(input_path)
     column_labels = list(df.columns)
     env_column_index = column_labels.index('env')
-    approach_column_index = column_labels.index('approach')
+    approach_column_index = column_labels.index('approach_id')
     accuracy_column_index = column_labels.index('success')
-    experiment_name_index = column_labels.index('experiment_id')
 
     for _, row_raw in df.iterrows():
         row = row_raw.tolist()
@@ -41,18 +36,13 @@ def _construct_dictionaries(input_path: str) -> Tuple[Dict, Dict]:
         environment_name = row[env_column_index]
         accuracy = row[accuracy_column_index]
         accuracy = float(accuracy[0:accuracy.index(' ')])
-        experiment_name = row[experiment_name_index]
-        for approach in baseline_approaches:
-            if approach_name == "pure-planning" and approach in experiment_name:
-                baseline_approaches[approach][environment_name] = accuracy
-        for approach in custom_approaches:
+        for approach in APPROACHES:
             if approach_name == approach:
-                custom_approaches[approach][environment_name] = accuracy
-    return custom_approaches, baseline_approaches
+                approaches[approach][environment_name] = accuracy
+    return approaches
 
 
-def _get_visualization(custom_approaches: Dict[str, Dict[str, str]],
-                       baseline_approaches: Dict[str, Dict[str, str]],
+def _get_visualization(approaches: Dict[str, Dict[str, str]],
                        output_path: str) -> None:
     fig, axs = plt.subplots(nrows=5, ncols=6, figsize=(30, 25))
     plt.subplots_adjust(left=0.1,
@@ -61,14 +51,11 @@ def _get_visualization(custom_approaches: Dict[str, Dict[str, str]],
                         top=0.9,
                         wspace=0.4,
                         hspace=0.4)
-    for ax, env in zip(axs.flat,
-                       custom_approaches[CUSTOM_APPROACHES[0]].keys()):
-        llm_approaches = CUSTOM_APPROACHES + BASELINE_APPROACHES
+    for ax, env in zip(axs.flat, approaches[APPROACHES[0]].keys()):
+        llm_approaches = APPROACHES
         llm_accuracies = []
-        for approach in custom_approaches:
-            llm_accuracies.append(custom_approaches[approach][env])
-        for approach in baseline_approaches:
-            llm_accuracies.append(baseline_approaches[approach][env])
+        for approach_dict in approaches.values():
+            llm_accuracies.append(approach_dict[env])
         ax.bar(llm_approaches, llm_accuracies, color="blue")
         ax.set_title(env)
         ax.set_ylabel("accuracy")
