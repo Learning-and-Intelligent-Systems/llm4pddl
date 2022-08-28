fb6043e8487f68c76f6de97958e88ebd2195698b
Tom Silver <tomssilver@gmail.com>
2022-08-28 09:31:55 -0400

    add setup.py to autoformatting list

--- run_autoformat.sh
+++ run_autoformat.sh
@@ -2,5 +2,6 @@
 yapf -i -r --style .style.yapf --exclude '**/third_party' llm4pddl
 yapf -i -r --style .style.yapf scripts
 yapf -i -r --style .style.yapf tests
+yapf -i -r --style .style.yapf *.py
 docformatter -i -r . --exclude venv llm4pddl/third_party
 isort .
