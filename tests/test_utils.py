406b6336bea3a03bc7970a7c4d667977f4318044
Tom Silver <tomssilver@gmail.com>
2022-08-31 18:31:18 -0400

    fix test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -194,9 +194,40 @@ def test_minify_pddl_problem():
     """
     utils.reset_flags({"llm_prompt_flatten_pddl": False})
     # Testing big example #1:
-    task01 = utils.get_task_from_dir(utils.CUSTOM_BENCHMARK_DIR / 'dressed', 1)
-    with open(task01.problem_file, 'r', encoding='utf-8') as f:
-        problem01 = f.read()
+    problem01 = """(define (problem dressed)
+  (:domain dressed)
+  (:objects person1 person2 person3 person4 - person
+            dress1 - dress
+            sweatpants1 sweatpants2 - sweatpants
+            sweatshirt1 sweatshirt2 - sweatshirt
+            nice-pants1 - nice-pants
+            collared-shirt1 - collared-shirt
+            suit-jacket1 - suit-jacket
+            )
+  (:init (wearing-nothing-formal person1)
+         (wearing-nothing-casual person1)
+         (wearing-nothing-formal person2)
+         (wearing-nothing-casual person2)
+         (wearing-nothing-formal person3)
+         (wearing-nothing-casual person3)
+         (wearing-nothing-formal person4)
+         (wearing-nothing-casual person4)
+         (in-closet dress1)
+         (in-closet sweatpants1)
+         (in-closet sweatpants2)
+         (in-closet sweatshirt1)
+         (in-closet sweatshirt2)
+         (in-closet nice-pants1)
+         (in-closet collared-shirt1)
+         (in-closet suit-jacket1)
+         )
+  (:goal (and (attending-casual-event person3)
+              (attending-formal-event person1)
+              (attending-casual-event person4)
+              (attending-formal-event person2)
+              ))
+  )
+"""
     expected01 = """(define (problem dressed)
 (:domain dressed)
 (:objects person1 person2 person3 person4 - person
