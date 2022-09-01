ef7ca48cc814a1a5f469b6e618db4cf648653cc9
Tom Silver <tomssilver@gmail.com>
2022-09-01 10:33:49 -0400

    wip

--- scripts/create_gripper_scaling_plot.py
+++ scripts/create_gripper_scaling_plot.py
@@ -36,33 +36,13 @@ def _main() -> None:
     assert (df.success == 1.0).all()
     # Create plot.
     plt.figure()
-    sns.lineplot(data=df, x="num_balls", y="solve_time", hue="approach_id")
+    sns.lineplot(data=df,
+                 x="num_balls",
+                 y="solve_time",
+                 hue="approach_id",
+                 style="approach_id",
+                 markers=True)
     plt.savefig("test.png")
-
-
-def _get_visualization(approaches: Dict[str, Dict[str, str]],
-                       output_path: str) -> None:
-    fig, axs = plt.subplots(nrows=5, ncols=6, figsize=(30, 25))
-    plt.subplots_adjust(left=0.1,
-                        bottom=0.1,
-                        right=0.9,
-                        top=0.9,
-                        wspace=0.4,
-                        hspace=0.4)
-    for ax, env in zip(axs.flat, approaches[APPROACHES[0]].keys()):
-        llm_approaches = APPROACHES
-        llm_accuracies = []
-        for approach_dict in approaches.values():
-            llm_accuracies.append(approach_dict[env])
-        ax.bar(llm_approaches, llm_accuracies, color="blue")
-        ax.set_title(env)
-        ax.set_ylabel("accuracy")
-        ax.set_xticklabels(llm_approaches, rotation=45)
-    fig.savefig(output_path,
-                facecolor='w',
-                bbox_inches="tight",
-                pad_inches=0,
-                transparent=True)
 
 
 if __name__ == "__main__":
