0dabfb9523657ff07a0c207ac8346f84676e8597
Tom Silver <tomssilver@gmail.com>
2022-09-02 14:58:14 -0400

    make an option to continue or break when a bad action is hit in init-queue

--- llm4pddl/flags.py
+++ llm4pddl/flags.py
@@ -36,7 +36,7 @@ def parse_flags() -> None:
     parser.add_argument("--llm_max_total_tokens", default=4096, type=int)
     parser.add_argument("--llm_prompt_method", default="standard", type=str)
     parser.add_argument("--llm_plan_guidance_method",
-                        default="init-queue",
+                        default="init-queue-continue",
                         type=str)
     parser.add_argument('--debug',
                         action="store_const",
