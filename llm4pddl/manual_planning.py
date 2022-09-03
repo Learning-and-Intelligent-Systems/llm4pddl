e72486f4a0e97afa0fbfe30af94bbe41c47da185
Tom Silver <tomssilver@gmail.com>
2022-09-03 12:32:35 -0400

    manual policy for gripper

--- llm4pddl/manual_planning.py
+++ llm4pddl/manual_planning.py
@@ -1,6 +1,6 @@
 """Domain-specific plan creation."""
 
-from typing import Collection, List
+from typing import Collection, Dict, List, Set
 
 from llm4pddl import utils
 from llm4pddl.structs import Plan, PyperplanObject, PyperplanPredicate, Task
@@ -8,9 +8,11 @@ from llm4pddl.structs import Plan, PyperplanObject, PyperplanPredicate, Task
 
 def create_manual_plan(task: Task, env_name: str) -> Plan:
     """Generate a plan for the task using env-specific code."""
-
-    assert "blocks" in env_name
-    return _create_manual_blocks_plan(task)
+    if "blocks" in env_name:
+        return _create_manual_blocks_plan(task)
+    if "gripper" in env_name:
+        return _create_manual_gripper_plan(task)
+    raise NotImplementedError(f"Manual plan not implemented for {env_name}")
 
 
 def _create_manual_blocks_plan(task: Task) -> Plan:
@@ -63,5 +65,80 @@ def _create_manual_blocks_plan(task: Task) -> Plan:
             top = remaining_pile[0]
             plan.append(f"(pick-up {top})")
             plan.append(f"(stack {top} {bottom})")
+
+    return plan
+
+
+def _create_manual_gripper_plan(task: Task) -> Plan:
+    _, problem = utils.parse_task(task)
+
+    # Parse the balls.
+    def _atoms_to_ball_locs(
+            atoms: Collection[PyperplanPredicate]) -> Dict[str, str]:
+        ball_to_loc = {}
+        for atom in atoms:
+            if atom.name.lower() != "at":
+                continue
+            (ball, _), (loc, _) = atom.signature
+            ball_to_loc[ball] = loc
+        return ball_to_loc
+
+    # Parse the grippers.
+    def _atoms_to_grippers(atoms: Collection[PyperplanPredicate]) -> Set[str]:
+        free_grippers = set()
+        all_grippers = set()
+        for atom in atoms:
+            if atom.name.lower() == "free":
+                (gripper, _), = atom.signature
+                free_grippers.add(gripper)
+            elif atom.name.lower() == "gripper":
+                (gripper, _), = atom.signature
+                all_grippers.add(gripper)
+        # Assume all grippers start out free.
+        assert free_grippers == all_grippers
+        return all_grippers
+
+    # Parse the robot.
+    def _atoms_to_robot_loc(atoms: Collection[PyperplanPredicate]) -> str:
+        at_robby = [a for a in atoms if a.name.lower() == "at-robby"]
+        assert len(at_robby) == 1
+        return at_robby[0].signature[0][0]
+
+    # Assumes that all balls start out in the same place.
+    init_ball_locs = _atoms_to_ball_locs(problem.initial_state)
+    assert len(init_ball_locs) > 0
+    init_ball_loc = next(iter(init_ball_locs.values()))
+    assert all(l == init_ball_loc for l in init_ball_locs.values())
+    grippers = sorted(_atoms_to_grippers(problem.initial_state))
+    robot_loc = _atoms_to_robot_loc(problem.initial_state)
+    assert robot_loc == init_ball_loc
+    # Assumes that all goal balls need to go to the same place.
+    goal_ball_locs = _atoms_to_ball_locs(problem.goal)
+    goal_ball_loc = next(iter(goal_ball_locs.values()))
+    assert all(l == goal_ball_loc for l in goal_ball_locs.values())
+
+    # Construct the plan.
+    plan = []
+
+    remaining_balls = sorted(goal_ball_locs)
+    while remaining_balls:
+        gripper_to_ball = {}
+        for gripper in grippers:
+            if not remaining_balls:  # pragma: no cover
+                break
+            gripper_to_ball[gripper] = remaining_balls.pop(0)
+        # Picks.
+        for gripper in sorted(gripper_to_ball):
+            ball = gripper_to_ball[gripper]
+            plan.append(f"(pick {ball} {init_ball_loc} {gripper})")
+        # Move.
+        plan.append(f"(move {init_ball_loc} {goal_ball_loc})")
+        # Place.
+        for gripper in sorted(gripper_to_ball):
+            ball = gripper_to_ball[gripper]
+            plan.append(f"(drop {ball} {goal_ball_loc} {gripper})")
+        # If there are more to go, move back.
+        if remaining_balls:
+            plan.append(f"(move {goal_ball_loc} {init_ball_loc})")
 
     return plan
