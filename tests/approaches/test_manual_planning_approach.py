db7b37637ca6f69da081c07d00b98f1f295999f1
Tom Silver <tomssilver@gmail.com>
2022-09-04 14:44:04 -0400

    coverage

--- tests/approaches/test_manual_planning_approach.py
+++ tests/approaches/test_manual_planning_approach.py
@@ -14,8 +14,8 @@ from llm4pddl.envs import create_env
 def test_manual_planning_approach(env_name):
     """Tests for ManualPlanningApproach()."""
     utils.reset_flags({
-        "num_train_tasks": 0,
-        "num_eval_tasks": 2,
+        "num_train_tasks": 2,
+        "num_eval_tasks": 1,
         "train_task_offset": 0,
         "planner": "pyperplan",
         "planning_timeout": 100
