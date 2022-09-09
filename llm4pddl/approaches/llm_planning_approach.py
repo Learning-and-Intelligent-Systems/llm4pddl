d8015c22e353461a97bd72a3d0d487ba69ba35b8
Tom Silver <tomssilver@gmail.com>
2022-09-09 09:35:52 -0400

    fix the completely broken implementation of llm plan with autoregress

--- llm4pddl/approaches/llm_planning_approach.py
+++ llm4pddl/approaches/llm_planning_approach.py
@@ -1,12 +1,11 @@
 """An approach that uses the output of an LLM to guide planning."""
 
-import logging
 from typing import List, Optional, Tuple
 
 from llm4pddl import utils
 from llm4pddl.approaches.llm_open_loop_approach import LLMOpenLoopApproach
 from llm4pddl.flags import FLAGS
-from llm4pddl.structs import LLMResponse, Plan, Task, TaskMetrics
+from llm4pddl.structs import Plan, Task, TaskMetrics
 
 
 class LLMPlanningApproach(LLMOpenLoopApproach):
@@ -19,17 +18,9 @@ class LLMPlanningApproach(LLMOpenLoopApproach):
     def get_name(self) -> str:
         return "llm-plan"
 
-    def _llm_responses_to_plan(
-            self, responses: List[LLMResponse],
+    def _solve_from_partial_plans(
+            self, partial_plans: List[Plan],
             task: Task) -> Tuple[Optional[Plan], TaskMetrics]:
-        # Turn each response into a sequence of actions. Do not check the
-        # preconditions of the actions; rely on the planner to do that.
-        partial_plans = []
-        for response in responses:
-            logging.debug(f"Processing response:\n{response.response_text}")
-            partial_plan = self._llm_response_to_plan(response.response_text,
-                                                      task)
-            partial_plans.append(partial_plan)
         # Use the partial plans to guide the planner.
         return utils.run_pyperplan_planning(
             task,
