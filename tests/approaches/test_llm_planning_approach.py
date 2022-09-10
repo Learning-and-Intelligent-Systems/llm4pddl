19792f75020474645891b83200b550ac6db1e2a6
Alchemy5 <varunhpr5@gmail.com>
2022-09-10 19:44:33 -0400

    resolved coverage issue

--- tests/approaches/test_llm_planning_approach.py
+++ tests/approaches/test_llm_planning_approach.py
@@ -34,7 +34,8 @@ def test_llm_planning_planning_approach():
         "use_dynamic_examples": False,
         "data_dir": data_dir,
         "load_data": False,
-        "embedding_model_name": "paraphrase-MiniLM-L6-v2"
+        "embedding_model_name": "paraphrase-MiniLM-L6-v2",
+        "random_object_names": False
     })
     env = create_env("pyperplan-gripper")
     train_tasks = env.get_train_tasks()
