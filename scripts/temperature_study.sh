ed78029abb40e8d9dd3e431a8c888f7e7f9cb175
Alchemy5 <varunhpr5@gmail.com>
2022-09-15 18:24:58 -0400

    commented out/temp

--- scripts/temperature_study.sh
+++ scripts/temperature_study.sh
@@ -1,13 +1,13 @@
 FILE="llm4pddl/main.py"
 NUM_TRAIN_TASKS="2"
-SEEDS = (
+SEEDS=(
     "0"
     "1"
     "2"
 )
-ENVS = (
-    "gripper"
-    "logistics"
+ENVS=(
+#    "gripper"
+#    "logistics"
     "miconic"
     "scanalyzer"
     "woodworking"
@@ -15,22 +15,22 @@ ENVS = (
     "parcprinter"
     "movie"
 )
-APPROACH = "llm-multi"
-TEMPERATURES = (
+APPROACH="llm-multi"
+TEMPERATURES=(
     "0.0"
     "0.3"
     "0.5"
     "0.8"
     "0.9"
 )
-RESULTS_DIRS = (
+RESULTS_DIRS=(
     "results/0"
     "results/3"
     "results/5"
     "results/8"
     "results/9"
 )
-CACHE_DIRS = (
+CACHE_DIRS=(
     "llm_cache/0"
     "llm_cache/3"
     "llm_cache/5"
@@ -41,25 +41,25 @@ CACHE_DIRS = (
 for ENV in ${ENVS[@]}; do
     for SEED in ${SEEDS[@]}; do
         for TEMP in ${TEMPERATURES[@]}; do
-            if [$TEMP = "0.0"]; then
-                RESULT_DIR = "results/0"
-                CACHE_DIR = "llm_cache/0"
+            if [ $TEMP = "0.0" ]; then
+                RESULT_DIR="results/0"
+                CACHE_DIR="llm_cache/0"
             fi
-            if [$TEMP = "0.3"]; then
-                RESULT_DIR = "results/3"
-                CACHE_DIR = "llm_cache/3"
+            if [ $TEMP = "0.3" ]; then
+                RESULT_DIR="results/3"
+                CACHE_DIR="llm_cache/3"
             fi
-            if [$TEMP = "0.5"]; then
-                RESULT_DIR = "results/5"
-                CACHE_DIR = "llm_cache/5"
+            if [ $TEMP = "0.5" ]; then
+                RESULT_DIR="results/5"
+                CACHE_DIR="llm_cache/5"
             fi
-            if [$TEMP = "0.8"]; then
-                RESULT_DIR = "results/8"
-                CACHE_DIR = "llm_cache/8"
+            if [ $TEMP = "0.8" ]; then
+                RESULT_DIR="results/8"
+                CACHE_DIR="llm_cache/8"
             fi
-            if [$TEMP = "0.9"]; then
-                RESULT_DIR = "results/9"
-                CACHE_DIR = "llm_cache/9"
+            if [ $TEMP = "0.9" ]; then
+                RESULT_DIR="results/9"
+                CACHE_DIR="llm_cache/9"
             fi
             python $FILE --env pyperplan-${ENV} --approach $APPROACH --seed $SEED --data_gen_planner pyperplan --llm_multi_temperature $TEMP --results_dir $RESULT_DIR --llm_cache_dir $CACHE_DIR
         done
